4cf0fb2cdb68c3e002104f5588c839e00fc72dd3
Apple <apple@users-MacBook.home>
2016-08-23 13:08:33 -0700

    Feed UI completed

--- J2 Social/CircleView.swift
+++ J2 Social/CircleView.swift
@@ -1,26 +1,30 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  J2 Social
 //
-//  Created by Apple on 8/17/16.
+//  Created by Apple on 8/23/16.
 //  Copyright © 2016 J2Jenkins. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
-    override func awakeFromNib()
-    {
+    override func awakeFromNib() {
+        
         super.awakeFromNib()
+        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: SHADOW_GRAY).cgColor
         
-       layer.cornerRadius = 2.0
+        
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
         
     }
-
 }
